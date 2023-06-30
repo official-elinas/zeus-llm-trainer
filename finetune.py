fde62c7e80a09f7cbc40d600474a3be6d6a13f1e
Elinas <support@sneerz.com>
2023-06-29 20:39:47 -0500

    add --use_rope option and make double_quant not default for now

--- finetune.py
+++ finetune.py
@@ -195,7 +195,7 @@ def train(
         nf4_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,
+            # bnb_4bit_use_double_quant=True,
             bnb_4bit_compute_dtype=torch.bfloat16
         )
         model = LlamaForCausalLM.from_pretrained(
